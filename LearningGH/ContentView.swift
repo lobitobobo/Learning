ef6a327b97ad652256fa7535602bea8cf901ed1a
lobitobobo <87725183+lobitobobo@users.noreply.github.com>
2023-08-06 22:46:49 -0400

    Adapte content view to whtasip

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world repo! New version")
+            Text("What'sup, world repo! New version")
         }
         .padding()
     }
